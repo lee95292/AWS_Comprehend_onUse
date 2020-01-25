3defee711f4cd6b58d9da2d335dbac6d36860859
lee95292 <lee95292@naver.com>
2020-01-26 01:28:03 +0900

    get keyphrase function is substituted to anchor tag

--- src/web/index.js
+++ src/web/index.js
@@ -1,9 +1,11 @@
 const xhr = new XMLHttpRequest();
 
-xhr.open(
-  "GET",
-  "https://newsapi.org/v2/top-headlines?sortBy=publishedAt&country=us&apiKey=99ef42fc4cf0491c928597b08fd47354"
-);
+const newsUrl =
+  "https://newsapi.org/v2/top-headlines?sortBy=publishedAt&country=us&apiKey=99ef42fc4cf0491c928597b08fd47354";
+const lambdaUrl =
+  "https://78oq0tcw6j.execute-api.us-east-1.amazonaws.com/ss/Comprehend_API";
+let articles;
+xhr.open("GET", newsUrl);
 
 xhr.send();
 
@@ -12,12 +14,11 @@ xhr.onreadystatechange = function(e) {
   // readyState: 4 => DONE(서버 응답 완료)
   if (xhr.readyState !== XMLHttpRequest.DONE) return;
 
-  // status는 response 상태 코드를 반환 : 200 => 정상 응답
   if (xhr.status === 200) {
-    const articles = JSON.parse(xhr.responseText).articles;
+    articles = JSON.parse(xhr.responseText).articles;
     const content = document.getElementById("news");
 
-    articles.forEach(element => {
+    articles.forEach((element, index) => {
       let hr = document.createElement("hr");
       let titleLink = document.createElement("a");
       titleLink.appendChild(document.createTextNode(element.title));
@@ -36,8 +37,32 @@ xhr.onreadystatechange = function(e) {
           .createElement("p")
           .appendChild(document.createTextNode("Date: " + element.publishedAt))
       );
+      uri = encodeURI(lambdaUrl + "?content=" + articles[index].description);
+      console.log(uri);
+      content.innerHTML +=
+        "<a target='_new' href=" + uri + ">get key phrases</a>";
     });
   } else {
     console.log("Error!");
   }
 };
+
+// function getKeyPhrase(index) {
+//   const lambdaUrl =
+//     "https://78oq0tcw6j.execute-api.us-east-1.amazonaws.com/ss/Comprehend_API";
+//   let desc = "set the origin";
+//   if (index != -1) {
+//     desc = articles[index].description;
+//   }
+//   const xhr = new XMLHttpRequest();
+//   xhr.open("GET", lambdaUrl + "?content=" + desc);
+//   xhr.send();
+//   xhr.onreadystatechange = function(e) {
+//     if (xhr.readyState !== XMLHttpRequest.DONE) return;
+
+//     if (xhr.status === 200) {
+//       const keywords = JSON.parse(xhr.responseText).KeyPhrases;
+//       console.log(keywords);
+//     }
+//   };
+// }
